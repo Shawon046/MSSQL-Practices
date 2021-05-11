0c859f999e034969c8df2f6d02694f1c3373feb2
shawon046 <shawon56512@gmail.com>
2021-05-11 17:40:21 +0600

    fixed "log in" link from "sign up" tab

--- signinup.js
+++ signinup.js
@@ -1,4 +1,4 @@
 var module = angular.module('myApp', []);
 module.controller('MyCtrl', function($scope) {
-  $scope.tabName = "tab1";
+  $scope.tabName = "tab1"; // always shows the "log in" tab first
 });
