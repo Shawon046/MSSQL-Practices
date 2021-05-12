a0a497fe639b960f47b99a342022e5c0cc568a72
SkAditAziz <38491593+SkAditAziz@users.noreply.github.com>
2021-05-12 15:24:31 +0600

    database connected

--- server.js
+++ server.js
@@ -7,6 +7,44 @@ app.use('/static', express.static('signinup'))
 
 app.get('/',function(req, res) {
     res.sendFile(path.join(__dirname,'signinup/signin-signup.html'))
+
+    console.log("requiring mssql....")
+
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        server: 'localhost\\MSSQLSERVER', 
+        database: 'workStation',
+        user: 'sa',
+        password: 'loosely_coupled',
+        port : 1433,
+        trustServerCertificate: true
+    };
+
+    function getUsers() {
+        var conn = new sql.ConnectionPool(config);
+        var req = new sql.Request(conn);
+
+        conn.connect(function(err){
+            if(err){
+                console.log(err);
+                return;
+            }
+            req.query("SELECT * FROM USERS",function(err,recordset){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log(recordset);
+                }
+                conn.close();
+            });
+        });
+    }
+
+    
+
 });
 
 app.listen(3000);
