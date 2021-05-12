2bbf8edcdf245fc38c9a903328d19374951ee28b
shawon046 <shawon56512@gmail.com>
2021-05-12 11:52:46 +0600

    adding 2 controllers for getting user name, pass from  signin-signup.html file

--- signinup.js
+++ signinup.js
@@ -1,4 +1,79 @@
-var module = angular.module('myApp', []);
-module.controller('MyCtrl', function($scope) {
-  $scope.tabName = "tab1"; // always shows the "log in" tab first
+// var module = angular.module('loginApp', []);
+// module.controller('loginCtrl', function($scope) {
+  
+// });
+
+// this segment is added by Shawon
+var app = angular.module("loginApp", ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+  //inject $locationProvider service
+  $locationProvider.hashPrefix(''); 
+  // add configuration
+	$routeProvider
+		.when('/', {
+			templateUrl: 'signin-signup.html',
+			controller: 'loginCtrl'
+		})
+		.when('/sign', {
+			templateUrl: 'signin-signup.html'
+			// controller: 'SecondController'
+		})
+    .when('/dash', {
+			templateUrl: 'dashboard.html'
+			// controller: 'SecondController'
+		})
+    .when('/check', {
+			templateUrl: './Login Page/welcome.html',
+			// controller: 'SecondController'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
+
+
+app.controller('tabCtrl', function($scope, $location){
+  // always shows the "log in" tab first
+  $scope.tabName = "tab1"; 
+  // console.log($scope);
+  
+
+});
+
+
+app.controller('loginCtrl', function($scope, $location){
+  // // always shows the "log in" tab first
+  // $scope.tabName = "tab1"; 
+  // console.log($scope);
+
+  
+  $scope.submit = function(){
+    var uname = $scope.username ;
+    var pass = $scope.password ;
+    console.log(uname,pass);
+
+    console.log($location.absUrl());
+    if( uname ==  'babu@gmail' && pass== '2'){
+        $location.path('/dash');
+        console.log($location.absUrl());
+      }else{
+        alert('Wrong Username or Password. Please try again!');
+      }
+  };
+
+  $scope.register = function(){
+    console.log( `hi`);
+    var uname = $scope.name ;
+    var email = $scope.email ;
+    var country = $scope.country ;
+    var phone = $scope.phone ; 
+    var pass = $scope.password ;
+    console.log(uname, email, country, phone, pass);
+    console.log($location.absUrl());
+    var url = $location.url('/sign');
+    console.log($location.absUrl());
+  };
+
+
 });
