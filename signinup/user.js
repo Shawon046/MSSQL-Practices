c656c546638f8cc59ccbb821a320315cc5e4874c
SkAditAziz <38491593+SkAditAziz@users.noreply.github.com>
2021-05-18 12:07:24 +0600

    getUser method serving

---
+++ signinup/user.js
@@ -0,0 +1,11 @@
+class User{
+    constructor(Id,Title,Quantity,Message,City){
+        this.Id = Id; 
+        this.Title = Title; 
+        this.Quantity = Quantity;
+        this.Message = Message;
+        this.City = City; 
+    }
+}
+
+export default User;
