145610953892686b52506b0042b7960cf280b892
shawon046 <shawon56512@gmail.com>
2021-05-19 00:48:35 +0600

    server connected to static view

--- REST API Approach/serevr.js
+++ REST API Approach/serevr.js
@@ -4,39 +4,54 @@ const path = require('path');
 
 // const ejs = require('ejs');
 
-// init app
+// Init App
 const app = express();
 
-// setting view
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine', 'ejs');
+// Bring in Models
+// let { Article } = require('./models/article');
+
+// Load View Engine
+// app.set('views',path.join(__dirname,'views'));
+// app.set('view engine', 'ejs');
+
+app.use('/static', express.static( 'views'));
+
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+// parse application/json
+app.use(express.json());
+
 
 // home route
 
 // Passing Server-Side Data to the Frontend
+// For a simple array, you can use this example instead, 
+// which will create a p tag for every name in the listnames variable
 // app.get('/', function (req, res) {
 //     var name = "Louise";
+//     var listnames = ["Louise", "Sadie", "Erik", "Raph", "Gina"];
 //     // Render index page
 //     res.render('index', {
 //         // EJS variable and server-side variable
 //         title: 'article', 
 //         message: 'Hello there!' ,
-//         name: name
+//         name: name,
+//         listnames: listnames
 //     });
 // });
 
-// For a simple array, you can use this example instead, 
-// which will create a p tag for every name in the listnames variable
+
 app.get('/', function (req, res) {
-    var name = "Louise";
-    var listnames = ["Louise", "Sadie", "Erik", "Raph", "Gina"];
     // Render index page
-    res.render('index', {
-        // EJS variable and server-side variable
-        title: 'article', 
-        message: 'Hello there!' ,
-        name: name,
-        listnames: listnames
+    fileName = 'login.html' ;
+    res.sendFile(path.join(__dirname,'views/'+fileName), function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
     });
 });
 
